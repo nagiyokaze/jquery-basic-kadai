2315b4e075a3d728b356f30f684b3129dbfeab05
nagiyokaze <158305309+nagiyokaze@users.noreply.github.com>
2024-05-08 20:57:54 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(function(){
+$(window).on('load',function(){
+        console.log('loadイベントが発生しました');
+    });
+
+$(window).on('scrool',function(){
+    console.log('scrollイベントが発生しました');
+    });
+});
